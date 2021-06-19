c3c76494b316680cb8eb2e29cdcfdf3ada9a12bd
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-19 19:22:53 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,5 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+  extend MetaDancing
 end
