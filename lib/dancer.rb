6e536f7782d3eabadb087e84e2d4cc3a3aec3161
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-19 19:14:28 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,16 +2,6 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  extend MetaDancing
-  end
-
-  include Dance
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-
-end
-end
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
