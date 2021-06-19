def74f9075932b40c99a358c1c60798469222001
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-19 19:28:52 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,7 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dancer < Dance
+class Dancer 
   extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
